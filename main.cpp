6822e065d46963ac2edba4c90444c718f2c1005c
Manish-butola <79547520+Manish-butola@users.noreply.github.com>
2021-02-24 13:38:36 -0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+
+     int i=0,j=0,k=0;
+    for(i=1;i<=5;i++)
+    {
+        cout<<"\n";
+     for(j=1;j<=i;j++)
+       cout<<" ";
+
+     for(k=i;k<=5;k++)
+         cout<<k;
+     }
+
+
+}
